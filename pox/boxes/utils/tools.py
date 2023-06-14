241fb0e8c983b05b98fccb934561b5a713c12fc5
Lionel Metongnon <lionel.metongnon@uclouvain.be>
2023-06-14 17:03:28 +0200

    List of open ports added to the device policy and Alert update with flowheader

--- pox/boxes/utils/tools.py
+++ pox/boxes/utils/tools.py
@@ -1,23 +1,25 @@
 import time
 
 class Policy(object):
-	def __init__(self, deviceIP, maxPacketSize, maxFlowNumber):
+	def __init__(self, deviceIP, maxPacketSize, maxFlowNumber, openPorts=None):
 		self.deviceIP = deviceIP
 		self.maxPacketSize = maxPacketSize
 		self.maxFlowNumber = maxFlowNumber
+		self.openPorts = openPorts
 
 	def __eq__(self, other):
 		if not isinstance(other, Policy): return False
 		if self.deviceIP != other.deviceIP: return False
 		if self.maxPacketSize != other.maxPacketSize: return False
 		if self.maxFlowNumber != other.maxFlowNumber: return False
+		if self.openPorts != other.openPorts: return False
 		return True
 
 	def __ne__(self, other):
 		return not self.__eq__(self, other)
 
 	def __str__(self):
-		return "[deviceIP %s, maxPacketSize %s, maxFlowNumber %s]", (self.deviceIP, self.maxPacketSize, self.maxFlowNumber)
+		return "[deviceIP %s, maxPacketSize %s, maxFlowNumber %s, openPorts %s]", (self.deviceIP, self.maxPacketSize, self.maxFlowNumber, self.openPorts)
 
 class Permission(object):
 	def __init__(self, decision, duration, maxPacketSize):
@@ -43,15 +45,27 @@ class Permission(object):
 		return "decision:%s, duration:%s, max flow size:%s, %timestamp:%s" % (self.decision, self.duration, self.maxPacketSize, self.timestamp)
 
 class Alert(object):
-	def __init__(self, deviceIP, alertType, flow=None):
+	COMPLIANCE	= 0
+	DOS 		= 1
+	DDOS 		= 2
+	SCAN 		= 3
+	
+	def __init__(self, deviceIP, alertType, flowHeader, flow=None):
 		self.deviceIP 	= deviceIP
 		self.alertType 	= alertType
+		self.flowHeader = flowHeader if flowHeader is not None else FlowHeader.fromMatch(flow)
 		self.flow 		= flow
+		# self._init(flowHeader, flow)
+
+	# def _init(self, flowHeader, flow):
+	# 	if flowHeader is None and not flow is None:
+	# 		self.flowHeader = FlowHeader.fromMatch(flow)
 
 	def __eq__(self, other):
 		if not isinstance(other, Alert): return False
 		if self.deviceIP != other.deviceIP: return False
 		if self.alertType != other.alertType: return False
+		if self.flowHeader != other.flowHeader: return False
 		if self.flow != other.flow: return False
 		return True
 
@@ -59,4 +73,4 @@ class Alert(object):
 		return not self.__eq__(self, other)
 
 	def __str__(self):
-		return "[deviceIP %s, alertType %s, flow %s]" % (self.deviceIP, self.alertType, self.flow)
+		return "[deviceIP %s, alertType %s, flowHeader %s, flow %s]" % (self.deviceIP, self.alertType, self.flowHeader, self.flow)
