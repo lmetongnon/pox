a34d210d7717dec9284701250a367f72ad3be59b
Lionel Metongnon <lionel.metongnon@uclouvain.be>
2023-10-10 18:25:49 +0200

    Settings configuration for TopBoxe

--- pox/boxes/tbox.py
+++ pox/boxes/tbox.py
@@ -1,13 +1,18 @@
+from pox.core import core
+
 from pox.lib.addresses import IPAddr, IPAddr6
 from pox.boxes.manager import Tmanager
 
+log = core.getLogger('tbox')
+
 class Tbox(object):
 
-	def __init__(self, ip, port):
+	def __init__(self, ip, port, version, filename):
 		self.ip = ip
 		self.port = port
-		self.messageManager = Tmanager(self, ip, port)
+		self.messageManager = Tmanager(self, ip, port, version, filename)
 
 # def launch(ip=IPAddr6("2000:db8::1"), port=15700):
-def launch(ip="192.168.0.1", port=15700):
-	Tbox(ip, port)
+def launch(box_ip="2.2.0.1", box_port=15700, version=4, filename=None):
+	log.debug("launch %s %s %s %s" % (box_ip, box_port, version, filename))
+	Tbox(box_ip, box_port, version, filename)
